ea05eb8c618785f2ea0799262f8b845549f5e3ef
Dean Katsaros <djkatsaros@gmail.com>
2025-01-11 10:49:31 -0800

    Update main.rs with comments

--- rust/6kyu/backspaces_in_string/src/main.rs
+++ rust/6kyu/backspaces_in_string/src/main.rs
@@ -17,21 +17,23 @@ fn main() {
 
 fn clean_string(s: &str) -> String {
     let mut out: Vec<char> = Vec::new();
-    for c in s.chars() {
+    for c in s.chars() { // iterate over the char iterater. 
         if c == '#' {
             if out.len() > 0 {
-                out.pop();
+                out.pop(); // delete last entry if current char is == '#'
             }
         } else {
-            out.push(c);
+            out.push(c); // push the char c otehrwise
         }
     }
-    let out_str: String = out.iter().collect();
+    let out_str: String = out.iter().collect(); // make an iterator, and collect into a string. (makes a string from a list)
     out_str
 }
 
 /*
- * fn clean_string(s: &str) -> String {
+ * // ALternate, slicker soluition using a fold. Essentially does the above in 1 line and without having to use collect after 
+fn clean_string(s: &str) -> String {
+     // fold the char iterator of s into acc(umulator) based on the conditional that we pop (delete last entry) of acc if the current char c is == '#', and add c otherwise
     s.chars().fold(String::new(), |mut acc, c| if c == '#' {acc.pop(); acc} else {acc.push(c); acc})
 }
  */
