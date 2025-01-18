906455925916347e31c0829bc9f2e9a49525d50e
Dean Katsaros <djkatsaros@gmail.com>
2025-01-17 19:49:06 -0800

    In the land of the blind the one eyed man is king

---
+++ rust/6kyu/playing_w_digits/src/main.rs
@@ -0,0 +1,72 @@
+/*
+ * Some numbers have funny properties. For example:
+
+    89 --> 8¹ + 9² = 89 * 1
+    695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+    46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+Given two positive integers n and p, we want to find a positive integer k, if it exists, such that the sum of the digits of n raised to consecutive powers starting from p is equal to k * n.
+
+In other words, writing the consecutive digits of n as a, b, c, d ..., is there an integer k such that :
+(ap+bp+1+cp+2+dp+3+...)=n∗k(a^p + b^{p + 1} + c^{p + 2} + d^{p + 3} + ...) = n * k(ap+bp+1+cp+2+dp+3+...)=n∗k
+
+If it is the case we will return k, if not return -1.
+
+Note: n and p will always be strictly positive integers.
+ */
+
+fn digits(num: i64) -> impl Iterator<Item = u32> {
+    num.to_string()
+        .chars()
+        .map(|d| d.to_digit(10).unwrap())
+        .collect::<Vec<_>>()
+        .into_iter()
+}
+
+fn dig_pow(n: i64, p: i32) -> i64 {
+    let mut pow_ = p;
+    let mut s = 0u64;
+    for i in digits(n) {
+        let i = i as u64;
+        s = s + i.pow(pow_.try_into().unwrap());
+
+        pow_ = pow_ + 1;
+    }
+    // your code
+    let ss = s as u64;
+    let nn = n as u64;
+    if ss % nn == 0 {
+        (ss / nn) as i64
+    }
+    else {
+        -1
+    }
+ }
+
+#[cfg(test)]
+    mod tests {
+    use super::*;
+
+    fn dotest(n: i64, p: i32, exp: i64) -> () {
+        println!(" n: {:?};", n);
+        println!("p: {:?};", p);
+        let ans = dig_pow(n, p);
+        println!(" actual:\n{:?};", ans);
+        println!("expect:\n{:?};", exp);
+        println!(" {};", ans == exp);
+        assert_eq!(ans, exp);
+        println!("{};", "-");
+    }
+
+    #[test]
+    fn basic_tests() {
+        dotest(89, 1, 1);
+        dotest(92, 1, -1);
+        dotest(46288, 3, 51);
+
+    }
+}
+
+fn main() {
+    println!("Play with digits");
+}
